3e7816ada8269546c6396e57282401d536569d83
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2023-01-12 11:01:22 +0000

    [Fix]本の投稿エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
 
   def ensure_correct_user
     @user = User.find(params[:id])
-    unless @user == current_user
+    unless @user =! current_user
       redirect_to user_path(current_user)
     end
   end
