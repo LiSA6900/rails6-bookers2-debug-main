2b515318fd3d43309643ec7869605aab64d56484
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2023-01-15 15:17:43 +0000

    [Add]フォロー機能のビュー追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,26 +5,26 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
-	redirect_to request.referer
+	  redirect_to request.referer
   end
   
   # フォロー外すとき
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-	redirect_to request.referer
+	  redirect_to request.referer
   end
   
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-	@users = user.followings
+	  @users = user.followings
   end
   
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-	@users = user.followers
+	  @users = user.followers
   end
   
 end
