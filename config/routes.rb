8a9a33f306829d1da7e2711e0e69305c79fded73
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2023-01-15 13:35:39 +0000

    [Add]フォロー機能のモデル・コントローラ・ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
     # ※resources → resource 「s」をなくすことで、URLに/:idが含まれなくなる。
     # いいね機能は1人のユーザーが1つの投稿にいいねを押すことが出来るのが１回のみなのでURLにparams[:id]を使わなくても良いので「ｓ」を無くした書き方にしている。
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    # ネストさせる
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
