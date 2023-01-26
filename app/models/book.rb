cc066cbb1700fc4696ddf4067ffd6ba4b23f9d23
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-01-26 13:48:27 +0000

    [Add]本の投稿一覧で過去1週間でいいねの合計が多い順に投稿表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,14 +3,16 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_belginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   
+  # favorited_by?メソッドを作成する事でユーザーidがfavoriteテーブル内に存在するかどうかを判別している（いいねを既に押しているか、押していないか）
+  # bookモデルで定義したものは後ほどviewで使用する。
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  # favorited_by?メソッドを作成する事でユーザーidがfavoriteテーブル内に存在するかどうかを判別している（いいねを既に押しているか、押していないか）
-  # bookモデルで定義したものは後ほどviewで使用する。
   
   def self.search_for(content, method)
     if method == 'perfect' # タイトルが完全一致
