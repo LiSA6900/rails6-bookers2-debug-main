fd8cab833f3a47a26d81f0f46e1f27f58a8faa88
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-02-24 15:06:03 +0000

    [Add]コメント追加

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -17,8 +17,8 @@ class EventMailer < ApplicationMailer
     group = event[:group]
     group.users.each do |member|
       # deliver_nowとdeliver_laterの２種類のメール送信メソッドがあります。
-      # deliver_nowは、今すぐに送信したい場合に使用します。
-      # deliver_laterは、非同期で送信したい場合に使用します。
+      # deliver_now：すぐに送信したい場合に使用します。
+      # deliver_later：非同期で送信したい場合に使用します。
       EventMailer.send_notification(member, event).deliver_now
     end
   end
