c56aa5a135677e63fa37c56098b8cee05295082a
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-03-02 14:07:48 +0000

    [Add]【7d】5段階評価の表示

--- db/migrate/20210727093149_create_books.rb
+++ db/migrate/20210727093149_create_books.rb
@@ -4,6 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :body
       t.integer :user_id
+      t.float :rate #【データ型】float : 浮動小数
       t.timestamps
     end
   end
